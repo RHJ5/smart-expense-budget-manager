b52bd9d69d4b3c4702140b68ca24a742bcdef9b5
RHJ5 <rafeyhafeez39@gmail.com>
2026-08-30 09:29:43 -0700

    feat: add dashboard summary API, seed categories script

---
+++ database/06_seed_default_categories.sql
@@ -0,0 +1,11 @@
+INSERT  INTO Categories (
+    name,
+    type,
+    isDefault,
+    userId
+)
+VALUES                 ('Salary', 'income', 1, NULL),
+('Freelance', 'income', 1, NULL),
+('Food', 'expense', 1, NULL),
+('Transport', 'expense', 1, NULL),
+('Bills', 'expense', 1, NULL);
